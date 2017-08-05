249ddffb6840ae2a6e5032e814b4082fa410d8ae
dantleech <dan.t.leech@gmail.com>
2017-08-05 19:41:41 +0200

    Added Phpactor completor

--- pythonx/completers/php.py
+++ pythonx/completers/php.py
@@ -9,30 +9,36 @@ from completor.compat import to_unicode
 
 class Php(Completor):
     filetype = 'php'
-    trigger = r'(?::\w{2,}\w*|->\w*)$'
+    trigger = r'(::\w*|->\w*)$'
 
     def offset(self):
         line, col = vim.current.window.cursor
         line2byte = vim.Function('line2byte')
-        return line2byte(line) + col - 3
+        return line2byte(line) + col - 1
 
     def format_cmd(self):
         binary = self.get_option('phpactor_binary') or 'phpactor'
         return [binary, 'complete', self.tempname, self.offset(), '--format=json']
 
     def parse(self, data):
+        if len(data) == 0:
+            return []
+
         res = []
         data = to_unicode(data[0], 'utf-8')
-        data = json.loads(data)
 
-        if 'error' in data:
-            pprint.pprint(data)
+        try:
+            data = json.loads(data)
+        except json.decoder.JSONDecodeError:
+            return []
+
+        if not 'suggestions' in data:
             return []
 
         for item in data['suggestions']:
             res.append({
                 'word': item['name'],
-                'menu': item['type']
+                'menu': item['info']
             })
 
         return res
