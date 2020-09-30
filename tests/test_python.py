3549e297f990b33460f70064eb77acd24de04388
maralla <maralla.ai@gmail.com>
2020-09-30 14:58:53 +0800

    Fix test

--- tests/test_python.py
+++ tests/test_python.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import completor
 
 def test_python_jedi_requests(vim_mod):
     import jedi
     from completers.python.python_jedi import JediProcessor
-    
-    args = {'action': 'complete', 'line': 0, 'col': 28, 'filename': '1.py', 'content': 'import requests; requests.ge'}
+
+    args = {
+        'action': 'complete',
+        'line': 0,
+        'col': 28,
+        'filename': '1.py',
+        'content': 'import requests; requests.ge'
+    }
+
     processor = JediProcessor(jedi)
-    try:
-        import requests
-        assert processor.process(args)[0]['menu'] == 'def get'
-    except ImportError:
-        assert processor.process(args)[0]['menu'] == 'get = api.get'
+    items = processor.process(args)
+
+    menus = [i['menu'] for i in items]
+
+    assert 'def get' in menus
